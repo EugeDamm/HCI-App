68a18050c906cf76c7ebfdda66f8f8eb64fe21cd
EugeDamm <euge.damm@gmail.com>
2019-05-30 09:38:10 -0300

    added rooms

--- app/src/main/java/ar/edu/itba/hci/smarthomesystem/MainActivity.java
+++ app/src/main/java/ar/edu/itba/hci/smarthomesystem/MainActivity.java
@@ -3,6 +3,7 @@ package ar.edu.itba.hci.smarthomesystem;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+<<<<<<< HEAD
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
@@ -33,14 +34,57 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
     };
+=======
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
 
+import java.awt.font.TextAttribute;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+>>>>>>> parent of 1c97c7a... Added rooms fragment
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.rooms:
+                break;
+            case R.id.routines:
+                break;
+            case R.id.alarm:
+                fragment = new Alarm();
+        }
+        return loadFragment(fragment);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
+<<<<<<< HEAD
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+=======
+        navView.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if(fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+>>>>>>> parent of 1c97c7a... Added rooms fragment
 }
