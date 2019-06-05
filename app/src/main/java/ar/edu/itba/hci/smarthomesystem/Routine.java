acf431df9e6ad32b099ad5ecff3952e93ba2114b
Udemy Rails Course <mcraviotto@itba.edu.ar>
2019-06-04 22:30:14 -0300

    GET method done

--- app/src/main/java/ar/edu/itba/hci/smarthomesystem/Routine.java
+++ app/src/main/java/ar/edu/itba/hci/smarthomesystem/Routine.java
@@ -1,16 +1,22 @@
 package ar.edu.itba.hci.smarthomesystem;
 
-import java.lang.reflect.Array;
+import org.json.JSONArray;
 
 public class Routine {
 
     String id;
     String name;
-    String[] actions;
+    JSONArray actions;
 
-    public Routine(String id, String name, String[] actions) {
+    public Routine(String id, String name, JSONArray actions) {
         this.id = id;
         this.name = name;
         this.actions = actions;
+    }
+
+
+    @Override
+    public String toString() {
+        return id + " " + name;
     }
 }
--- app/src/main/java/ar/edu/itba/hci/smarthomesystem/Routines.java
+++ app/src/main/java/ar/edu/itba/hci/smarthomesystem/Routines.java
@@ -1,7 +1,6 @@
 package ar.edu.itba.hci.smarthomesystem;
 
 
-import android.icu.text.AlphabeticIndex;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -15,7 +14,16 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,26 +35,75 @@ import java.util.List;
 public class Routines extends Fragment {
 
     private LinearLayout linearLayout = null;
+    private RequestQueue mQueue;
+
 
     public Routines() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_routines, container , false);
+        mQueue = Volley.newRequestQueue(getContext());
+        getRoutines();
+        return inflater.inflate(R.layout.fragment_routines, container, false);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         linearLayout = (LinearLayout) view.findViewById(R.id.linearLayout);
+    }
 
-        final List<Routine> routines = getRoutines();
-        if (routines.size() == 0) {
+    private void makeActions(Routine routine) {
+        // TODO: Tendria que hacer las acciones de la rutina aca.
+        Log.d("MAKEACTION", routine.id);
+    }
+
+
+    public void getRoutines() {
+
+        final List<Routine> result = new ArrayList<>();
+        String url = "http://10.0.2.2:8080/api/routines";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray routines = response.getJSONArray("routines");
+                            for (int i=0 ; i < routines.length(); i++) {
+                                JSONObject routine = routines.getJSONObject(i);
+                                String id = routine.getString("id");
+                                String name = routine.getString("name");
+                                JSONArray actions = routine.getJSONArray("actions");
+                                Routine nextToAdd = new Routine(id, name, actions);
+                                // Log.d("ROUTINE", nextToAdd.toString());
+                                result.add(nextToAdd);
+                            }
+                            updateRoutines(result);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        Log.d("B", response.toString());
+                    }
+
+
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                Log.d("ERROR", error.toString());
+            }
+        });
+        mQueue.add(request);
+    }
+
+    private void updateRoutines(final List<Routine> list) {
+        if (list.size() == 0) {
             TextView label = new TextView(getActivity());
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
             params.topMargin = 20;
@@ -57,11 +114,11 @@ public class Routines extends Fragment {
             label.setText(R.string.no_routines);
             linearLayout.addView(label);
         }
-        for (int i = 0; i < routines.size(); i++) {
+        for (int i = 0; i < list.size(); i++) {
             final int index = i;
             //Create your Controls(UI widget, Button,TextView) and add into layout
             Button btn = new Button(getActivity());
-            btn.setText(routines.get(i).name);
+            btn.setText(list.get(i).name);
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
             params.topMargin = 20;
             params.leftMargin = 20;
@@ -74,28 +131,10 @@ public class Routines extends Fragment {
                     String name = button.getText().toString();
                     Log.d("CLICK", name);
                     Toast.makeText(getContext(), name + " On", Toast.LENGTH_LONG).show();
-                    makeActions(routines.get(index));
+                    makeActions(list.get(index));
                 }
             });
             linearLayout.addView(btn);
         }
     }
-
-    private List<Routine> getRoutines() {
-        List<Routine> list = new ArrayList<>();
-        // TODO: I should ask the API for the routines, and transform each into a Routine.class, and add it to the ArrayList
-        // Example ->
-        String array[] = {}; // This are the actions to make.
-        Routine example = new Routine("1234", "Routine 1", array);
-        Routine example2 = new Routine("5678", "Routine 2", array);
-        list.add(example);
-        list.add(example2);
-        return list;
-    }
-
-    private void makeActions(Routine routine) {
-        // TODO: Tendria que hacer las acciones de la rutina aca.
-        Log.d("MAKEACTION", routine.id);
-    }
-
 }
