bef156c715b28a0c03e9c082bf03aeaa7c1b88f0
R0L02796 <alejandro.rolandelli@gmail.com>
2019-06-10 15:12:19 -0300

    speechrecogniser 2

--- app/src/main/java/ar/edu/itba/hci/smarthomesystem/MainActivity.java
+++ app/src/main/java/ar/edu/itba/hci/smarthomesystem/MainActivity.java
@@ -1,13 +1,23 @@
 package ar.edu.itba.hci.smarthomesystem;
 
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,6 +40,8 @@ import api.Error;
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
     private final String LOG_TAG = "ar.edu.itba.apiexample";
     private static final String TAG = "MainActivity";
+    private static final int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
+    private SpeechRecognizer speechRecog;
     private ArrayList<Room> rooms;
     private Fragment fragment = null;
     private Bundle bundle = new Bundle();
@@ -54,6 +66,12 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                 handleError(error);
             }
         });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(OnClickAction());
+
+        initializeSpeechRecognizer();
+
     }
 
     @Override
@@ -123,4 +141,106 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         Toast.makeText(MainActivity.this, text, Toast.LENGTH_LONG).show();
     }
 
+
+    private void initializeSpeechRecognizer() {
+
+        if (SpeechRecognizer.isRecognitionAvailable(this)) {
+            speechRecog = SpeechRecognizer.createSpeechRecognizer(this);
+            speechRecog.setRecognitionListener(new RecognitionListener() {
+                @Override
+                public void onReadyForSpeech(Bundle params) {
+
+                }
+
+                @Override
+                public void onBeginningOfSpeech() {
+
+                }
+
+                @Override
+                public void onRmsChanged(float rmsdB) {
+
+                }
+
+                @Override
+                public void onBufferReceived(byte[] buffer) {
+
+                }
+
+                @Override
+                public void onEndOfSpeech() {
+
+                }
+
+                @Override
+                public void onError(int error) {
+
+                }
+
+                @Override
+                public void onResults(Bundle results) {
+                    List<String> resultArr = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                    processResult(resultArr.get(0));
+                }
+
+                @Override
+                public void onPartialResults(Bundle partialResults) {
+
+                }
+
+                @Override
+                public void onEvent(int eventType, Bundle params) {
+
+                }
+            });
+        }
+    }
+
+    private void processResult(String resultMessage) {
+        resultMessage = resultMessage.toLowerCase();
+        if (resultMessage.contains("a")) {
+            Log.v(TAG,"anda");
+
+        }
+
+    }
+
+    private View.OnClickListener OnClickAction() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Here, thisActivity is the current activity
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.RECORD_AUDIO)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    // Permission is not granted
+                    // Should we show an explanation?
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                            Manifest.permission.RECORD_AUDIO)) {
+                        // Show an explanation to the user *asynchronously* -- don't block
+                        // this thread waiting for the user's response! After the user
+                        // sees the explanation, try again to request the permission.
+                    } else {
+                        // No explanation needed; request the permission
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{Manifest.permission.RECORD_AUDIO},MY_PERMISSIONS_REQUEST_RECORD_AUDIO);
+
+                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                        // app-defined int constant. The callback method gets the
+                        // result of the request.
+                    }
+                } else {
+                    // Permission has already been granted
+                    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                    intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,1);
+                    speechRecog.startListening(intent);
+
+                    Log.v(TAG,"click");
+
+                }
+            }
+        };
+    }
 }
