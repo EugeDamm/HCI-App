9435f73445b2dac9ac82a4e953da50257c3145d2
bautiblacker <bblacker@itba.edu.ar>
2019-06-16 21:18:50 -0300

    Notifications added

--- app/src/main/java/ar/edu/itba/hci/smarthomesystem/MainActivity.java
+++ app/src/main/java/ar/edu/itba/hci/smarthomesystem/MainActivity.java
@@ -80,6 +80,28 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                         }
                     });
                     return;
+                case "alarm":
+                    Api.getInstance(MainActivity.this).getDevices(new Response.Listener<ArrayList<Device>>() {
+                        @Override
+                        public void onResponse(ArrayList<Device> response) {
+                            for (Device d : response) {
+                                if (d.getTypeId().equals(ALARM_TYPE_ID)) {
+                                    AlarmFragment alarmFragment = new AlarmFragment();
+                                    Bundle bundle = new Bundle();
+                                    Alarm alarm = new Alarm("", d.getName(), d.getId());
+                                    bundle.putSerializable("alarm", alarm);
+                                    alarmFragment.setArguments(bundle);
+                                    loadFragment(alarmFragment);
+                                }
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            ErrorHandler.handleError(error, MainActivity.this);
+                        }
+                    });
+                    return;
             }
         }
 
